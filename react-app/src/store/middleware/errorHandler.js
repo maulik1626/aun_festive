2ad6457c9278cfc8898acb6f43b4ba350a38e301
maulik-raja <rajamaulik16@gmail.com>
2025-09-28 22:45:24 +0530

    fix(state): make middleware valid Redux middleware functions

--- react-app/src/store/middleware/errorHandler.js
+++ react-app/src/store/middleware/errorHandler.js
@@ -1,1 +1,10 @@
-export default function errorHandler(){ }
+export default function errorHandler(){
+  return (next) => (action) => {
+    try {
+      return next(action);
+    } catch (err) {
+      // no-op placeholder
+      throw err;
+    }
+  };
+}
