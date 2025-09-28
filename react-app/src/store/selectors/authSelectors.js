d4b93dcaaaf68eb0b35b04e038008ae534a3e6fd
maulik-raja <rajamaulik16@gmail.com>
2025-09-28 23:05:02 +0530

    chore(selectors): finalize auth/order selectors

--- react-app/src/store/selectors/authSelectors.js
+++ react-app/src/store/selectors/authSelectors.js
@@ -2,7 +2,6 @@ import { createSelector } from "reselect";
 
 const selectAuth = (state) => state.auth;
 export const selectUser = createSelector([selectAuth], (auth) => auth.user);
-export const selectIsAuthenticated = createSelector(
-  [selectAuth],
-  (auth) => Boolean(auth.token)
+export const selectIsAuthenticated = createSelector([selectAuth], (auth) =>
+  Boolean(auth.token)
 );
--- react-app/src/store/selectors/orderSelectors.js
+++ react-app/src/store/selectors/orderSelectors.js
@@ -2,4 +2,7 @@ import { createSelector } from "reselect";
 
 const selectOrders = (state) => state.orders;
 export const selectOrdersList = createSelector([selectOrders], (o) => o.list);
-export const selectOrdersStatus = createSelector([selectOrders], (o) => o.status);
+export const selectOrdersStatus = createSelector(
+  [selectOrders],
+  (o) => o.status
+);
