e02bfd8382942571cf34ce091535e0d6e3ad69db
maulik-raja <rajamaulik16@gmail.com>
2025-09-28 23:03:22 +0530

    feat(state): add memoized selectors; docs: check Step 6 and Step 7

--- react-app/src/store/selectors/authSelectors.js
+++ react-app/src/store/selectors/authSelectors.js
@@ -1,1 +1,8 @@
-export {};
+import { createSelector } from "reselect";
+
+const selectAuth = (state) => state.auth;
+export const selectUser = createSelector([selectAuth], (auth) => auth.user);
+export const selectIsAuthenticated = createSelector(
+  [selectAuth],
+  (auth) => Boolean(auth.token)
+);
--- react-app/src/store/selectors/productSelectors.js
+++ react-app/src/store/selectors/productSelectors.js
@@ -1,1 +1,11 @@
-export {};
+import { createSelector } from "reselect";
+
+const selectProducts = (state) => state.products;
+export const selectProductList = createSelector(
+  [selectProducts],
+  (p) => p.list
+);
+export const selectProductsStatus = createSelector(
+  [selectProducts],
+  (p) => p.status
+);
